789764aed7b9c3fbc421916f7955e9df3598200d
agnes-kabaly <alicet.woods@gmail.com>
2017-02-26 20:16:24 +0100

    tej pack.

---
+++ src/tej/FeliratosTej.java
@@ -0,0 +1,13 @@
+package tej;
+
+import java.util.Date;
+
+import bolt.Tej;
+
+public class FeliratosTej extends Tej {
+
+	public FeliratosTej(long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+		super(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+	}
+
+}
---
+++ src/tej/TartosTej.java
@@ -0,0 +1,13 @@
+package tej;
+
+import java.util.Date;
+
+import bolt.Tej;
+
+public class TartosTej extends Tej {
+
+	public TartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+		super(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+	}
+
+}
